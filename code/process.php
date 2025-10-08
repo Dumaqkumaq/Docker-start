cff6f290538f15b9771fa2078d901fa36f9636dd
Dyrdom4ik <atmakd7@gmail.com>
2025-10-09 00:09:01 +0300

    added simple error detector for email

--- code/process.php
+++ code/process.php
@@ -42,6 +42,17 @@ $email = htmlspecialchars($_POST['email'] ?? '');
 $_SESSION['username'] = $username;
 $_SESSION['email'] = $email;
 
+
+$errors = [];
+if(empty($username)) $errors[] = "Имя не может быть пустым";
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)) $errors[] = "Некорректный email";
+
+if(!empty($errors)){
+    $_SESSION['errors'] = $errors;
+    header("Location: index.php");
+    exit();
+}
+
 $line = $username . ";" . $email . "\n";
 file_put_contents("data.txt", $line, FILE_APPEND);
 
